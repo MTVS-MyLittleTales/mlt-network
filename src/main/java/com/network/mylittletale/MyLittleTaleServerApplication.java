d023a4c91325cdf9924e3993fea5f39914e683e8
sanghak <yee950419@naver.com>
2022-09-20 15:29:39 +0900

    초기 환경 세팅

---
+++ src/main/java/com/network/mylittletale/MyLittleTaleServerApplication.java
@@ -0,0 +1,13 @@
+package com.network.mylittletale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLittleTaleServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyLittleTaleServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/network/mylittletale/MyLittleTaleServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.network.mylittletale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLittleTaleServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
